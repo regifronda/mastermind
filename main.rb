d9faac1aa38f28da6479147eced0100a8ee907a9
Regi Fronda <regisfronda@gmail.com>
2021-06-30 04:57:29 -0700

    Add method codebreaker_mode

--- main.rb
+++ main.rb
@@ -2,10 +2,15 @@ module Mastermind
   class Game
     def initialize
       beginning_prompt
+      codebreaker_mode
     end
 
     def beginning_prompt
       puts "this first shows up when you play the game!"
+    end
+
+    def codebreaker_mode
+      puts "You're a codebreaker!"
     end
   end
 
